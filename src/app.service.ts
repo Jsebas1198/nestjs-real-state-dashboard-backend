2e8fd2afd0d8d77d271f8903969afcf892060463
Juan Sebastian Fernandez Saez <jsebas1198@protonmail.com>
2023-05-21 11:12:39 -0400

    Made test forddbb conection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Db } from 'mongodb';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MONGO') private database: Db) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUsers() {
+    const taskCollection = this.database.collection('users');
+    return taskCollection.find().toArray();
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,26 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MongoClient } from 'mongodb';
 import config from 'src/config';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, host, port, dbName } =
+          configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'MONGO',
